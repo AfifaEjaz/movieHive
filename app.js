a39750418d27639acffcc18ba1b02840af774f9b
Afifa <afifaejazz@gmail.com>
2024-07-21 23:45:00 +0500

    updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url';
 import path from 'path'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
